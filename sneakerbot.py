89e8ffdea06e8f3eee91905081f76e1514d11b1e
Naveen Bandarage <bandaragenaveen@gmail.com>
2019-11-26 20:39:17 +1300

    The url generation works was missing a . lmao

--- sneakerbot.py
+++ sneakerbot.py
@@ -1,2 +1,14 @@
+#https://www.adidas.co.nz/stan-smith-shoes/EE5818.html?forceSelSize=EE5818_630
 def UrlGenerator(model, size):
-BaseSize = 580 #This is saying the shoesize is 6.5
+    BaseSize = 530 #This is saying the shoesize is 6.5
+    ShoeSize = size - 4
+    ShoeSize = ShoeSize *20
+    Rawsize = ShoeSize + BaseSize
+    ShoeSizeCode = int(Rawsize)
+    URL = 'https://www.adidas.co.nz/' + str(model) + '.html?forceSelSize=' + str(model) + '_' + str(ShoeSizeCode)
+    return URL
+Model = raw_input('Model #')
+Size = input('Size: ')
+
+URL = UrlGenerator(Model, Size)
+print(str(URL))
